9fff7effc67121442b79a323f462d4e45693ff11
gautalok <alokgautamofficial99@gmail.com>
2023-09-09 22:17:39 +0530

    removed unwanted files and folders

--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e9207e899619ebe7"; }
+/******/ 	__webpack_require__.h = function() { return "7a8f4ab8bd28dfa8"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
@@ -82,11 +82,6 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 		};
 /******/ 	};
 /******/ }
-/******/ 
-/******/ /* webpack/runtime/nonce */
-/******/ !function() {
-/******/ 	__webpack_require__.nc = undefined;
-/******/ }();
 /******/ 
 /******/ }
 );
