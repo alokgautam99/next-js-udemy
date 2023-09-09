f4698bf6e211e11e631e85a5caaba5adf1c2b7c2
Alok Gautam <88962633+alokgautam99@users.noreply.github.com>
2023-09-09 23:02:33 +0530

    emotion library installed (#4)

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7a8f4ab8bd28dfa8"; }
+/******/ 		__webpack_require__.h = function() { return "d085ae019c76ddb1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+  background-color: aliceblue;
+`;
+
 export default function Home() {
   return (
-    <>
-    </>
+    <Wrapper>
+      Emotion Library Installed
+    </Wrapper>
   )
 }
