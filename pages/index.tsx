09f399b744c709cc539617e66ef9e7ddd08a4ead
Alok Gautam <88962633+alokgautam99@users.noreply.github.com>
2023-09-17 01:07:01 +0530

    vercel branch deployment test (#3)
    * vercel branch deployment test
    
    * font changed for mobile
    
    * extra margin removed
    
    * text align centre
    
    * name changed
    
    * name changed
    
    * styled classnames added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,47 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
+import bgImage from "../assets/images/rose-bg.jpeg";
+import { mediaQuery } from "@/styledConstants";
+
+const Container = styled.div`
+  background-image: url("../assets/images/rose-bg.jpeg");
+`;
 
 const Wrapper = styled.div`
-  background-color: aliceblue;
+  width: 100%;
+  height: calc(100vh - 200px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  ${mediaQuery.uptoMobile}{
+    gap: 20px;
+  }
+`;
+
+const Message = styled.div`
+  font-size: 50px;
+  font-weight: 700;
+  color: #fff;
+  ${mediaQuery.uptoMobile}{
+    font-size: 30px;
+  }
 `;
 
 export default function Home() {
   return (
-    <Wrapper>
-      Emotion Library Installed
-    </Wrapper>
-  )
+    <Container
+      style={{
+        backgroundImage: `url(${bgImage.src})`,
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <Message>Hello Alok,</Message>
+      <Wrapper>
+        <Message>Good Morning</Message>
+        <Message>You're looking beautiful today</Message>
+      </Wrapper>
+    </Container>
+  );
 }
